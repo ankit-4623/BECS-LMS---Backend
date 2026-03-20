8049ae9602a97872a408a12d471d2141730e789b
Ankit Nayek <ankitnayek007@gmail.com>
2026-03-20 21:26:33 +0530

    fix the otp issue

--- mail_service/rabbitmqConfig.js
+++ mail_service/rabbitmqConfig.js
@@ -1,32 +1,33 @@
 import amqp from "amqplib";
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
 export const connectRabbitMQ = async () => {
   try {
-    const connection = await amqp.connect(process.env.RABBITMQ_URL, {
-      reconnect: true,
-      rejectUnauthorized: true,
-    });
+    const connection = await amqp.connect(process.env.RABBITMQ_URL);
     const channel = await connection.createChannel();
     const queueName = "send-otp";
     await channel.assertQueue(queueName, { durable: true });
     console.log("✅ Mail Service consumer started, listening for otp emails");
-
     await channel.consume(queueName, async (msg) => {
       if (msg !== null) {
         const { to, subject, body } = JSON.parse(msg.content.toString());
 
+        const mailOptions = {
+          from: `"BECS LMS" <${process.env.EMAIL}>`,
+          to,
+          subject,
+          text: body,
+        };
         try {
-          await resend.emails.send({
-            from: `BECS LMS <${process.env.EMAIL}>`,
-            to,
-            subject,
-            text: body,
-          });
+          await transporter.sendMail(mailOptions);
           channel.ack(msg);
           console.log(`✅ Mail sent successfully to: ${to}`);
         } catch (mailError) {
